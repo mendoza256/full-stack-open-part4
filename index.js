73e1153a832b83c0d7b0fbf7d46e9ae902574aa6
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-10-09 21:25:54 +0200

    add linting

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const config = require("./utils/config");
 const PORT = config.PORT;
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server running on port ${PORT}`);
 });
