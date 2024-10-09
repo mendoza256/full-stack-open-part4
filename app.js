0229122ff1b6254ecb152d1a2be9c1e7b6888b83
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-10-09 21:18:57 +0200

    exercise 4

---
+++ app.js
@@ -0,0 +1,28 @@
+const blogRouter = require("./controllers/blogs");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+
+const mongoUrl = config.MONGODB_URI;
+
+mongoose.connect(mongoUrl);
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("dist"));
+
+morgan.token("resbody", function (req) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms content: :resbody"
+  )
+);
+
+app.use("/api/blogs/", blogRouter);
+
+module.exports = app;
