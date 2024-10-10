0dbf0dac7536a0e7fc7bfa6bf753e5dbae656087
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-10-10 20:36:19 +0200

    until exercise 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,4 +1,6 @@
-const dummy = (blogs) => {
+const _ = require("lodash");
+
+const dummy = (_blogs) => {
   return 1;
 };
 
@@ -6,7 +8,36 @@ const totalLikes = (blogList) => {
   return blogList.reduce((sum, current) => sum + current.likes, 0);
 };
 
+const returnFavoriteBlog = (blogs) => {
+  const maxLikes = Math.max(...blogs.map((blog) => blog.likes));
+  const result = blogs.find((blog) => blog.likes === maxLikes);
+  return {
+    title: result.title,
+    author: result.author,
+    likes: result.likes,
+  };
+};
+
+const returnAuthorWithMostBlogs = (blogs) => {
+  const arr = _.sortBy(blogs, [
+    function (b) {
+      return b.author;
+    },
+  ]);
+
+  const blogsAmount = arr.filter(
+    (b) => b.author === arr[arr.length - 1].author
+  ).length;
+
+  return {
+    author: arr[arr.length - 1].author,
+    blogs: blogsAmount,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  returnFavoriteBlog,
+  returnAuthorWithMostBlogs,
 };
